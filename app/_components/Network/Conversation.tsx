c62809ceaf1f75815192ac8cf289ff2101ad1aea
hkyoo988 <hkyoo988@naver.com>
2024-07-04 13:23:14 +0000

    fix : 음성기록 첫번째 대화와 나머지 ui 통일

--- app/_components/Network/Conversation.tsx
+++ app/_components/Network/Conversation.tsx
@@ -7,15 +7,14 @@ interface ConversationProps {
 
 const Conversation: React.FC<ConversationProps> = ({ messages, className }) => {
   return (
-    <ul className={`max-w-md divide-y divide-gray-200 dark:divide-gray-700 ${className}`}>
+    <ul
+      className={`max-w-md divide-y divide-gray-200 dark:divide-gray-700 ${className}`}
+    >
       {messages.map((message, index) => {
         const [name, ...contentArr] = message.split(": ");
         const content = contentArr.join(": ");
         return (
-          <li
-            key={index}
-            className={index === 0 ? "pt-4 pb-0 sm:pt-5" : "py-4 sm:py-5"}
-          >
+          <li key={index} className="py-4 sm:py-5">
             <div className="flex items-center space-x-4 rtl:space-x-reverse">
               <div className="flex-1 min-w-0">
                 <p className="text-lg font-semibold text-gray-900 dark:text-white">
